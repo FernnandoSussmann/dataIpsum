73f32dd30a51d9763429cd619696db5538c4b34f
Fernnando Sussmann <fernnandoh@hotmail.com>
2017-12-11 20:21:45 -0200

    Implementation of float value

--- data_ipsum.py
+++ data_ipsum.py
@@ -11,11 +11,24 @@ def string_generate(size):
 
     return word
 
-def data_generate(_type, size):
-    if _type == "int":
-        return int_generate(size)
-    elif _type == "string":
-        return string_generate(size)
+def float_generate(integer_size, decimal_size):
+    _int = ""
+    _decimals = ""
+
+    for i in range(0, integer_size):
+        _int += str(unichr(random.randint(48,57)))
+    for i in range(0, decimal_size):
+        _decimals += str(unichr(random.randint(48,57)))
+
+    return float(_int + "." + _decimals)
+
+def data_generate(settings):
+    if settings[0] == "int":
+        return int_generate(settings[1])
+    elif settings[0] == "string":
+        return string_generate(settings[1])
+    elif  settings[0] == "float":
+        return float_generate(settings[1], settings[2])
 
 def main():
     table_cols = int(raw_input("Set the quantity of columns to be generate "))
@@ -24,31 +37,22 @@ def main():
     col_properties = []
 
     for i in range(0, table_cols):
-        _type = raw_input("Set the type of value: " + str(i) + " ")
-        size = int(raw_input("Set the size of value: " + str(i) + " "))
+        settings = []
+        settings.append(raw_input("Set the type of value " + str(i) + ": "))
+        settings.append(int(raw_input("Set the size of value " + str(i) + ": ")))
 
-        col_properties.append([_type, size])
+        if settings[0] == "float":
+            settings.append(int(raw_input("Set the number of decimals " + str(i) + ": ")))            
+
+        col_properties.append([settings])
 
     for j in range(0, table_lines):
         col_line = []
         for i in range(0, table_cols):
             random.seed(i + j)
-            col_line.append(data_generate(col_properties[i][0], col_properties[i][1]))
+            col_line.append(data_generate(col_properties[i][0]))
         col_data.append(col_line)
     
     print col_data
-
-    # for j in range(0, table_lines):
-    #     for i in range(0, table_cols):
-    #         random.seed(i)
-    #         a = data_generate(col_properties[i][0], col_properties[i][1])
-    #         print a
-    #         col_line.append(a)
-    #     print col_line
-    #     col_data.append(col_line)
-
-    
-    # print col_line, "\n\n"
-    # print col_data
 
 main()
